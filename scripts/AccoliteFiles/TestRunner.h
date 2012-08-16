2841bbec85c021470eff97898f7f8ba4845da8c7
Romain Casati <romain.casati@inria.fr>
2012-08-16 11:54:16 +0200

    Avoid warning about unused parameters in TestRunner.h

--- scripts/AccoliteFiles/TestRunner.h
+++ scripts/AccoliteFiles/TestRunner.h
@@ -2,6 +2,8 @@
 #include <cppunit/TextTestRunner.h>
 int main( int argc, char **argv)
 {
+     (void) argc; // Avoid warning about unused parameter
+     (void) argv; // Avoid warning about unused parameter
      CppUnit::TextTestRunner runner;
      CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
      runner.addTest( registry.makeTest() );
