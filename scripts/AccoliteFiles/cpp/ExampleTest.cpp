ceae9274bf27ae69b3d43d869eae4832927e7722
Romain Casati <romain.casati@gmail.com>
2012-11-25 11:28:21 +0100

    GPL3 headers added on all files.

--- scripts/AccoliteFiles/cpp/ExampleTest.cpp
+++ scripts/AccoliteFiles/cpp/ExampleTest.cpp
@@ -1,3 +1,23 @@
+/*
+  Copyright (c) 2012 Casati Romain
+  All rights reserved.
+  
+  This file is part of Accolite.
+  
+  Accolite is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  Accolite is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with Accolite.  If not, see <http://www.gnu.org/licenses/>
+*/
+
 #include <cppunit/extensions/HelperMacros.h>
 
 #define <ACCOLITE_PROJECT_NAME_UPPER>_TEST_NAME ExampleTest
--- scripts/AccoliteFiles/cpp/PathsDefinitions.cmake
+++ scripts/AccoliteFiles/cpp/PathsDefinitions.cmake
@@ -1,3 +1,22 @@
+#
+# Copyright (c) 2012 Casati Romain
+# All rights reserved.
+#
+# This file is part of Accolite.
+#
+# Accolite is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Accolite is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Accolite.  If not, see <http://www.gnu.org/licenses/>
+
 ##########################
 # <ACCOLITE_PROJECT_NAME> path
 set( <ACCOLITE_PROJECT_NAME_UPPER>_PATH ${CMAKE_CURRENT_BINARY_DIR}/.. )
