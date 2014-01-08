eac38b04c3a8362ac45a69e48fc184d2a3165e3f
Romain Casati <romain.casati@gmail.com>
2014-01-08 16:27:34 +0100

    C/CPP-Unit not mandatory.

--- src/AccoliteFiles/c/proprietary/cmake/CMakeTestsLists.cmake
+++ src/AccoliteFiles/c/proprietary/cmake/CMakeTestsLists.cmake
@@ -26,32 +26,37 @@ set( NO_POLICY_SCOPE ON )
 
 ##########################
 # Dependencies
-find_package(CUnit REQUIRED)
+find_package(CUnit QUIET)
 
-##########################
-# Test files
-file(
-  GLOB_RECURSE
-  test_sources
-  ${TESTS_PATH}/*.c
-  )
-set_source_files_properties( ${test_sources}  PROPERTIES LANGUAGE C )
+if( CUNIT_FOUND )
+  ##########################
+  # Test files
+  file(
+    GLOB_RECURSE
+    test_sources
+    ${TESTS_PATH}/*.c
+    )
+  set_source_files_properties( ${test_sources}  PROPERTIES LANGUAGE C )
 
-##########################
-# Add tests
+  ##########################
+  # Add tests
 
-message( STATUS "Adding tests" )
-foreach( test ${test_sources} )
-  #string(REGEX REPLACE .c "" test_exe ${test} )
-  get_filename_component( test_exe ${test} NAME_WE)
-  add_executable( ${test_exe} ${test} )
-  target_link_libraries( ${test_exe} <ACCOLITE_PROJECT_NAME_LOWER> )
-  target_link_libraries( ${test_exe} ${CUNIT_LIBRARIES} )
-  include( TestsLinks.cmake )
-  add_test( ${test_exe} ${<ACCOLITE_PROJECT_NAME_UPPER>_CMD_TEST_OPTION} ${BIN_PATH}/${test_exe} )
-  message( STATUS "  ${test_exe} done" )
-endforeach( test ${test_sources} )
+  message( STATUS "Adding tests" )
+  foreach( test ${test_sources} )
+    #string(REGEX REPLACE .c "" test_exe ${test} )
+    get_filename_component( test_exe ${test} NAME_WE)
+    add_executable( ${test_exe} ${test} )
+    target_link_libraries( ${test_exe} <ACCOLITE_PROJECT_NAME_LOWER> )
+    target_link_libraries( ${test_exe} ${CUNIT_LIBRARIES} )
+    include( TestsLinks.cmake )
+    add_test( ${test_exe} ${<ACCOLITE_PROJECT_NAME_UPPER>_CMD_TEST_OPTION} ${BIN_PATH}/${test_exe} )
+    message( STATUS "  ${test_exe} done" )
+  endforeach( test ${test_sources} )
 
-##########################
-# Include CTestConfig.cmake
-include( CTest )
+  ##########################
+  # Include CTestConfig.cmake
+  include( CTest )
+else( CUNIT_FOUND )
+  message( STATUS
+    "WARNING: CUnit library not found, tests will not be compiled." )
+endif( CUNIT_FOUND )
--- src/AccoliteFiles/cpp/proprietary/cmake/CMakeTestsLists.cmake
+++ src/AccoliteFiles/cpp/proprietary/cmake/CMakeTestsLists.cmake
@@ -26,35 +26,40 @@ set( NO_POLICY_SCOPE ON )
 
 ##########################
 # Dependencies
-find_package(CppUnit REQUIRED)
+find_package(CppUnit QUIET)
 
-##########################
-# Test files
-file(
-  GLOB_RECURSE
-  test_sources
-  ${TESTS_PATH}/*.cpp
-  ${TESTS_PATH}/*.cxx
-  ${TESTS_PATH}/*.cc
-  ${TESTS_PATH}/*.c
-  )
-set_source_files_properties( ${test_sources}  PROPERTIES LANGUAGE CXX )
+if( CppUnit_FOUND )
+  ##########################
+  # Test files
+  file(
+    GLOB_RECURSE
+    test_sources
+    ${TESTS_PATH}/*.cpp
+    ${TESTS_PATH}/*.cxx
+    ${TESTS_PATH}/*.cc
+    ${TESTS_PATH}/*.c
+    )
+  set_source_files_properties( ${test_sources}  PROPERTIES LANGUAGE CXX )
 
-##########################
-# Add tests
+  ##########################
+  # Add tests
 
-message( STATUS "Adding tests" )
-foreach( test ${test_sources} )
-  #string(REGEX REPLACE .cpp "" test_exe ${test} )
-  get_filename_component( test_exe ${test} NAME_WE)
-  add_executable( ${test_exe} ${test} )
-  target_link_libraries( ${test_exe} <ACCOLITE_PROJECT_NAME_LOWER> )
-  target_link_libraries( ${test_exe} ${CppUnit_LIBRARIES} )
-  include( TestsLinks.cmake )
-  add_test( ${test_exe} ${<ACCOLITE_PROJECT_NAME_UPPER>_CMD_TEST_OPTION} ${BIN_PATH}/${test_exe} )
-  message( STATUS "  ${test_exe} done" )
-endforeach( test ${test_sources} )
+  message( STATUS "Adding tests" )
+  foreach( test ${test_sources} )
+    #string(REGEX REPLACE .cpp "" test_exe ${test} )
+    get_filename_component( test_exe ${test} NAME_WE)
+    add_executable( ${test_exe} ${test} )
+    target_link_libraries( ${test_exe} <ACCOLITE_PROJECT_NAME_LOWER> )
+    target_link_libraries( ${test_exe} ${CppUnit_LIBRARIES} )
+    include( TestsLinks.cmake )
+    add_test( ${test_exe} ${<ACCOLITE_PROJECT_NAME_UPPER>_CMD_TEST_OPTION} ${BIN_PATH}/${test_exe} )
+    message( STATUS "  ${test_exe} done" )
+  endforeach( test ${test_sources} )
 
-##########################
-# Include CTestConfig.cmake
-include( CTest )
+  ##########################
+  # Include CTestConfig.cmake
+  include( CTest )
+else( CppUnit_FOUND )
+  message( STATUS
+    "WARNING: CppUnit library not found, tests will not be compiled." )
+endif( CppUnit_FOUND )
